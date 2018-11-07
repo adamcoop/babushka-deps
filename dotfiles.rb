b97ad42b6265c0ac07cbc8a14f289f0a35daaf34
Adam Cooper <30548055+adamcoop@users.noreply.github.com>
2018-11-07 17:29:38 +1100

    Update dotfile repo source

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,7 +1,7 @@
 dep "personal dotfiles" do
   requires "stripysock:dotfiles"
   requires "stripysock:repository".with :path => "~/.dotfiles-personal",
-                                    :url => "https://github.com/jmacmullin/dotfiles.git",
+                                    :url => "https://github.com/adamcoop/dotfiles.git",
                                     :branch => "master"
 
   met? {
